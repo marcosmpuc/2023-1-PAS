033bef869f425107fd1ed2d2384f1623cf24494c
Isabella Becker <isabella.becker@edu.pucrs.br>
2023-05-21 19:33:46 -0300

    primeiro commit

---
+++ Exercício 5/micro-servico/src/test/java/com/projarq/microservico/MicroServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projarq.microservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
